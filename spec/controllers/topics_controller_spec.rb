1a5a9f3d82d0d8c5e39113d62a1b650cbbf0b4f7
Apprentice <apprentice@dbc-29.local>
2013-10-30 11:18:13 -0700

    working test for invalid params topics#create

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -7,5 +7,10 @@ describe TopicsController do
         post :create, topic: {title: "Meat", content: "It is what is for food"}
       }.to change { Topic.count }.by(1)
     end
+    it "doesn't create a topic with invalid params" do
+      expect {
+        post :create, topic: {title: "Wrong"}
+      }.to_not change { Topic.count }
+    end
   end
 end
