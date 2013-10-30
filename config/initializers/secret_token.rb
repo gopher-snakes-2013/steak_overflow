93b8aea1a36caab2eaeabaa89f69cbbaadc0782b
Apprentice <apprentice@dbc28.local>
2013-10-30 10:29:27 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SteakOverflow::Application.config.secret_token = 'b18be762e42087a5c80ab4b8749866632d95a1021bb87d54dceddb304ef28747663d3b7c4c1683b94d87386f2ff0c235be11ac4ad6136659c7f3eb9d1f12a298'
