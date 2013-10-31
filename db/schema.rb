c2f317f20e46860f3131877f1650f1319d4cd5c3
Apprentice <apprentice@dbc-29.local>
2013-10-30 17:12:56 -0700

    testing user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131030173459) do
+ActiveRecord::Schema.define(:version => 20131031000756) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
