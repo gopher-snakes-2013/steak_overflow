dc0750331ff7964977b332f01c828ecadf62ef02
Thomas Anthony <twa.photo@gmail.com>
2013-11-04 17:57:37 -0800

    heroku test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,22 +10,22 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 RSpec.configure do |config|
   config.include Capybara::DSL
 
-    config.mock_with :rspec
+  config.mock_with :rspec
 
-    config.before(:suite) do
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
-    end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-    config.before(:each, :js => true) do
-      DatabaseCleaner.strategy = :truncation
-    end
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
